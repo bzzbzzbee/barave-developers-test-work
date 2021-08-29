4d8744c6f3f0789e5f4aacdecb5fe00b66a04273
bzzbzzbee <chikipony666@gmail.com>
2021-08-29 19:02:50 +0700

    Splash screen

---
+++ app/src/main/java/com/example/bravedeveloperstestwork/fragments/SplashScreenFragment.kt
@@ -0,0 +1,47 @@
+package com.example.bravedeveloperstestwork.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.bravedeveloperstestwork.databinding.SplashScreenBinding
+
+class SplashScreenFragment : Fragment() {
+    private var _binding: SplashScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = SplashScreenBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                //val direction =
+                    //...
+                //findNavController().navigate(direction)
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {}
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {}
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
